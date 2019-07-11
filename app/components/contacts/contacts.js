16fef83180c040833b344520dd63d824b9fb8502
nick-everhard <everhard@fex.net>
2019-07-11 13:28:24 +0300

    Foobar

--- app/components/contacts/contacts.js
+++ app/components/contacts/contacts.js
@@ -1,7 +1,42 @@
 'use strict';
 
 $(function () {
-  $('.button-back').click(function () {
+  var Regex = {
+    EMAIL: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/
+  };
+
+  var $input = $('.contacts__input');
+  var $button = $('.contacts__button');
+  var $back = $('.button-back');
+
+  var checkValidityState = function () {
+    var predicat = true;
+
+    if ($input.hasClass('valid')) {
+      predicat = false;
+    }
+
+    $button.attr('disabled', predicat)
+  };
+
+  $input.on('input', function (evt) {
+    var regex = Regex.EMAIL;
+    var $this = $(this);
+    var value = $this.val();
+    evt.preventDefault();
+
+    checkValidityState();
+    if (!regex.test(value)) {
+      $this.removeClass('valid').addClass('invalid');
+      return;
+    }
+
+    $this.removeClass('invalid').addClass('valid');
+  });
+
+  checkValidityState();
+
+  $back.click(function () {
     if (window.history.length) {
       window.history.back();
       return;
