f296f083b5503f5ea7fe834a012eff98a374a327
mAfanasiev <m.afanasiev@everad.com>
2019-07-12 15:47:30 +0300

    fix

--- app/components/faq/faq.js
+++ app/components/faq/faq.js
@@ -2,6 +2,29 @@ $('.faq__item').click(function () {
    $(this).toggleClass('active').children('.faq__item-text').slideToggle("slow");
 });
 
-$('.faq__back').click(function () {
-    window.history.back();
+var hash;
+
+var exception = {
+    'faq': 1,
+    'general': 1,
+    'contacts': 1
+};
+
+if (reg.exec(window.location.pathname)) {
+    if(!exception[result[1]]) {
+        hash = result[1];
+        window.localStorage.setItem('hash', hash);
+    }
+} else {
+    window.localStorage.setItem('hash', 'index');
+}
+
+$('.faq__back--js').click(function (e) {
+    e.preventDefault();
+    if(window.localStorage.getItem('hash')) {
+        var val = window.localStorage.getItem('hash');
+        window.location.href = val +'.html';
+    } else {
+        window.location.href = 'index.html';
+    }
 });
